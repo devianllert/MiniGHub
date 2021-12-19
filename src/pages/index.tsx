e455691995e7061763f3683bbec3228f053731ac
Ruslan Povolockii <devianllert@gmail.com>
2021-12-19 16:13:56 +0300

    chore: add configs

---
+++ src/pages/index.tsx
@@ -0,0 +1,19 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Mini Github Client</title>
+        <meta name="description" content="Mini Github Client created with next" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div>123123</div>
+    </>
+  );
+};
+
+export default Home;
