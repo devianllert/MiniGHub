6710d7adff5a2ccd07d43065b5a2fb7f1d24923d
Ruslan Povolockii <devianllert@gmail.com>
2021-12-20 18:34:38 +0300

    feat: add react-query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,25 @@
+import * as React from 'react';
 import { AppProps } from 'next/app';
+import { QueryClientProvider, Hydrate, QueryClient } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 
+import { REACT_QUERY_STATE_PROP_NAME } from '@/modules/core/rquery/react-query';
 import { GlobalStyles } from '@/common/design/GlobalStyles';
 import { ResetStyles } from '@/common/design/ResetStyles';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [queryClient] = React.useState(() => new QueryClient());
+
   return (
-    <>
-      <GlobalStyles />
-      <ResetStyles />
-      <Component {...pageProps} />
-    </>
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps[REACT_QUERY_STATE_PROP_NAME]}>
+        <GlobalStyles />
+        <ResetStyles />
+        <Component {...pageProps} />
+
+        <ReactQueryDevtools initialIsOpen={false} />
+      </Hydrate>
+    </QueryClientProvider>
   );
 }
 
